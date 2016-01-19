54a6832a1563da2e7202fd22e116838019b8a500
Ayyappa Vemulkar <catchsat9@gmail.com>
2016-01-18 16:37:01 -0800

    limited info level messages to have max length of 2000 characters

--- lib/contextual_logging/logstash_message_formatter.rb
+++ lib/contextual_logging/logstash_message_formatter.rb
@@ -1,6 +1,7 @@
 module ContextualLogging
   class LogstashMessageFormatter
     def format(severity, message, extra_context)
+      message = message[0..1999] if severity == 'INFO'
       msg_hash = HashWithIndifferentAccess.new('message' => message, 'log_level' => severity)
       logstash_data = extra_context.merge(msg_hash)
       logstash_event = LogStash::Event.new(logstash_data)
@@ -8,4 +9,3 @@ module ContextualLogging
     end
   end
 end
-
