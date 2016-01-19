cc2cc42584d42c5c22548d5b6f2c250f712d8982
Ayyappa Vemulkar <catchsat9@gmail.com>
2016-01-19 14:15:09 -0800

    changed to create a new hash when severity is INFO

--- lib/contextual_logging/logstash_message_formatter.rb
+++ lib/contextual_logging/logstash_message_formatter.rb
@@ -1,9 +1,25 @@
 module ContextualLogging
   class LogstashMessageFormatter
     def format(severity, message, extra_context)
-      message = message[0..1999] if severity == 'INFO'
+      max_length = 2000
+
+      if severity == 'INFO'
+        message = message[0...max_length]
+        limited_extra_context = HashWithIndifferentAccess.new()
+        extra_context.each do |key,value|
+          if value.class == HashWithIndifferentAccess
+            limited_extra_context[key] = HashWithIndifferentAccess.new()
+            value.each do |k,v|
+              limited_extra_context[key][k] = v.inspect[0...max_length]
+            end
+          else
+            limited_extra_context[key] = value
+          end
+        end
+      end
+
       msg_hash = HashWithIndifferentAccess.new('message' => message, 'log_level' => severity)
-      logstash_data = extra_context.merge(msg_hash)
+      logstash_data = (severity == 'INFO') ? limited_extra_context.merge(msg_hash) : extra_context.merge(msg_hash)
       logstash_event = LogStash::Event.new(logstash_data)
       logstash_event.to_json
     end
--- spec/contextual_logging_spec.rb
+++ spec/contextual_logging_spec.rb
@@ -5,6 +5,19 @@ describe ContextualLogging::LogstashMessageFormatter do
     it 'should limit message size to 2000 characters when severity level is INFO' do
       message = (0...3000).map { ('a'..'z').to_a[rand(26)] }.join
       log_message = message_formatter.format('INFO', message, {})
-      expect(eval(log_message)[:message].length).to equal 2000
+      expect(JSON[log_message]['message'].length).to equal 2000
     end
+
+    it 'should limit extra context values that are two levels deep to 2000 characters when severity level is INFO' do
+      message = (0...3000).map { ('a'..'z').to_a[rand(26)] }.join
+      extra_context = HashWithIndifferentAccess.new()
+      extra_context['params'] = HashWithIndifferentAccess.new(nodes: {})
+      extra_context['text'] = (0...3000).map { ('a'..'z').to_a[rand(26)] }.join
+      extra_context['params']['nodes']['a'] = (0...3000).map { ('a'..'z').to_a[rand(26)] }.join
+
+      log_message = message_formatter.format('INFO', message, extra_context)
+      expect(JSON[log_message]['params']['nodes'].length).to equal 2000
+      expect(JSON[log_message]['text'].length).to equal 3000
+    end
+
 end
