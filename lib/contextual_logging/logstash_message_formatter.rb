313bcf74f698a00da721db86203c6b66930dcc7b
Ayyappa Vemulkar <catchsat9@gmail.com>
2016-01-22 15:38:11 -0800

    cleaned up stuff and broke things into smaller functions

--- lib/contextual_logging/logstash_message_formatter.rb
+++ lib/contextual_logging/logstash_message_formatter.rb
@@ -4,34 +4,43 @@ module ContextualLogging
 
     def format(severity, message, extra_context)
       if severity == 'INFO'
-        truncated_message = message[0...MAX_MESSAGE_LENGTH]
-        limited_extra_context = HashWithIndifferentAccess.new
-        extra_context.each do |key,value|
-          if value.is_a?(Hash)
-            limited_extra_context[key] = HashWithIndifferentAccess.new
-            value.each do |k,v|
-              if v.is_a?(String)
-                limited_extra_context[key][k] = v[0...MAX_MESSAGE_LENGTH]
-              else
-                limited_extra_context[key][k] = v.inspect[0...MAX_MESSAGE_LENGTH]
-              end
-            end
-          else
-            limited_extra_context[key] = value
-          end
-        end
+        truncated_message = truncate(message)
+        limited_extra_context = truncate_log_extra_context(extra_context)
+      else
+        truncated_message = message
+        limited_extra_context = extra_context
       end
 
-      if severity == 'INFO'
-        msg_hash = HashWithIndifferentAccess.new('message' => truncated_message, 'log_level' => severity)
-        logstash_data = limited_extra_context.merge(msg_hash)
-      else
-        msg_hash = HashWithIndifferentAccess.new('message' => message, 'log_level' => severity)
-        logstash_data = extra_context.merge(msg_hash)
-      end
+      logstash_data = HashWithIndifferentAccess.new(
+        'message' => truncated_message,
+        'log_level' => severity
+      ).merge(limited_extra_context)
 
       logstash_event = LogStash::Event.new(logstash_data)
       logstash_event.to_json
+    end
+
+    private
+
+    def truncate(message)
+      message[0...MAX_MESSAGE_LENGTH]
+    end
+
+    def truncate_log_extra_context(extra_context)
+      extra_context.each_with_object(HashWithIndifferentAccess.new) do |(key, value), limited_extra_context|
+        if value.is_a?(Hash)
+          limited_extra_context[key] = HashWithIndifferentAccess.new
+          value.each do |k, v|
+            if v.is_a?(String)
+              limited_extra_context[key][k] = truncate(v)
+            else
+              limited_extra_context[key][k] = truncate(v.inspect)
+            end
+          end
+        else
+          limited_extra_context[key] = value
+        end
+      end
     end
   end
 end
